bde3b7f075f5e3de6471bd0cb206ebe233481a0d
DannyIsYog <fabiodaniel.2000@gmail.com>
2022-04-06 14:44:06 +0100

    Added a wave system and exponential formulas for Health and currency

--- Assets/Scripts/EnemiesManager.cs
+++ Assets/Scripts/EnemiesManager.cs
@@ -9,6 +9,12 @@ public class EnemiesManager : MonoBehaviour
 
     [SerializeField] List<GameObject> spawnPoints;
 
+    [SerializeField] public int level;
+
+    [SerializeField] int waveSize;
+
+    [SerializeField] bool isBoss;
+
     public static EnemiesManager Instance { get; private set; }
 
     private void Awake()
@@ -26,26 +32,54 @@ public class EnemiesManager : MonoBehaviour
     }
     void Start()
     {
+        calculateWaveSize();
         queue = new List<GameObject>();
         foreach (GameObject spawnPoint in spawnPoints)
+        {
             queue.Add(Instantiate(enemiesList[Random.Range(0, enemiesList.Count)], spawnPoint.transform));
-
+            waveSize--;
+        }
     }
 
-    void spawnEntity()
+    void handleWave()
     {
+        if (waveSize == 0)
+        {
+            level++;
+            calculateWaveSize();
+        }
+        if (waveSize == 1) this.isBoss = true;
         queue.Add(Instantiate(enemiesList[Random.Range(0, enemiesList.Count)], spawnPoints[spawnPoints.Count - 1].transform));
+        waveSize--;
+
     }
 
     public void removeFirstInLine()
     {
+        //Removes enemy from the queue
         queue.RemoveAt(0);
+
+        //moves the enemy in the back to the front of the queue
         queue[0].transform.position = spawnPoints[0].transform.position;
-        spawnEntity();
+
+        handleWave();
     }
 
     public void damageFirstInLine()
     {
         PlayerManager.Instance.dealDamage(queue[0].GetComponent<Enemy>());
+    }
+
+    public bool isEnemyInFrontBoss()
+    {
+        if (!this.isBoss) return false;
+
+        this.isBoss = false;
+        return true;
+    }
+
+    void calculateWaveSize()
+    {
+        waveSize = Mathf.CeilToInt(10 * Mathf.Pow(1.1f, this.level));
     }
 }
--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Goblin : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    [SerializeField] int health;
+    [SerializeField] ulong health;
     [SerializeField] ulong currencyDrop;
 
     // Start is called before the first frame update
     void Start()
     {
+        int level = EnemiesManager.Instance.level;
+        bool isBoss = EnemiesManager.Instance.isEnemyInFrontBoss();
+
+        //Calculate Health
+        float a = Mathf.Pow(1.55f, level - 1);
+        this.health = (ulong)10 * (ulong)(level - 1 + Mathf.Pow(1.55f, level - 1));
+        if (isBoss) this.health *= 10;
+
+        //Calculate currencyDrop
+        this.currencyDrop = this.health / 10;
 
     }
 
@@ -19,14 +29,16 @@ public class Goblin : MonoBehaviour
 
     }
 
-    public void dealDamage(int damage)
+    public void dealDamage(ulong damage)
     {
         this.health -= damage;
-        if (this.health < 0)
-        {
-            CurrencyManager.Instance.addMonsterCurrency(currencyDrop);
-            EnemiesManager.Instance.removeFirstInLine();
-            Destroy(gameObject);
-        }
+        if (this.health == 0) die();
+    }
+
+    void die()
+    {
+        CurrencyManager.Instance.addMonsterCurrency(currencyDrop);
+        EnemiesManager.Instance.removeFirstInLine();
+        Destroy(gameObject);
     }
 }
